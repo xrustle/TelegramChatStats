8789a937a556bbfcb2ad56fe84d8a0d6e2306325
Dmitry Batorov <dbatorov@gmail.com>
2020-01-28 22:24:13 +0300

    collect_messages function added

--- bot/teleton.py
+++ bot/teleton.py
@@ -1,87 +1,28 @@
-from bot.config import API, MTPROTO
-from telethon import TelegramClient, sync, connection
-import time
+from bot.config import API, MTPROTO, CHATS
+from telethon import TelegramClient, connection
 import logging
-from pprint import pprint
+from bot.db import db
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-client = TelegramClient(
-    'session-telegram', **API,
-    connection=connection.ConnectionTcpMTProxyRandomizedIntermediate,
-    proxy=tuple(MTPROTO)
-)
 
+def collect_messages():
+    """
+    Используется для сбора всех новых сообщений из списка чатов указанных в конфиге
+    """
+    async def run():
+        async for dialog in client.iter_dialogs():
+            if dialog.id in CHATS:
+                print('Собираем недостающие сообщения для диалога', dialog.name)
+                async for message in client.iter_messages(dialog):
+                    if not db.insert_message(dialog.id, message.to_dict()):
+                        break
 
-async def main():
-    # Getting information about yourself
-    me = await client.get_me()
-
-    async for dialog in client.iter_dialogs():
-        if dialog.title == 'D+J':
-            async for message in client.iter_messages(dialog):
-                msg = message.to_dict()
-                if 'message' in msg:
-                    print(msg['message'])
-                else:
-                    print('<нет текста>')
-
-                time.sleep(.5)
-            members = await client.get_participants(dialog)
-            for member in members:
-                print(member.first_name, member.last_name)
-
-    # "me" is an User object. You can pretty-print
-    # any Telegram object with the "stringify" method:
-    # print(me.stringify())
-
-    # When you print something, you see a representation of it.
-    # You can access all attributes of Telegram objects with
-    # the dot operator. For example, to get the username:
-    # username = me.username
-    # print(username)
-    # print(me.phone)
-
-    # You can print all the dialogs/conversations that you are part of:
-    # async for dialog in client.iter_dialogs():
-    #     print(dialog.name, 'has ID', dialog.id)
-
-    # You can send messages to yourself...
-    # await client.send_message('me', 'Hello, myself!')
-    # ...to some chat ID
-    # await client.send_message(-100123456, 'Hello, group!')
-    # ...to your contacts
-    # await client.send_message('+34600123123', 'Hello, friend!')
-    # ...or even to any username
-    # await client.send_message('TelethonChat', 'Hello, Telethon!')
-
-    # You can, of course, use markdown in your messages:
-    # message = await client.send_message(
-    #     'me',
-    #     'This message has **bold**, `code`, __italics__ and '
-    #     'a [nice website](https://lonamiwebs.github.io)!',
-    #     link_preview=False
-    # )
-
-    # Sending a message returns the sent message object, which you can use
-    # print(message.raw_text)
-
-    # You can reply to messages directly if you have a message object
-    # await message.reply('Cool!')
-
-    # Or send files, songs, documents, albums...
-    # await client.send_file('me', '/home/me/Pictures/holidays.jpg')
-
-    # You can print the message history of any chat:
-    # async for message in client.iter_messages('me'):
-    #     print(message.id, message.text)
-
-        # You can download media from messages, too!
-        # The method will return the path where the file was saved.
-        # if message.photo:
-        #     path = await message.download_media()
-        #     print('File saved to', path)  # printed after download is done
-
-with client:
-    client.loop.run_until_complete(main())
+    with TelegramClient(
+            'session-telegram',
+            **API,
+            connection=connection.ConnectionTcpMTProxyRandomizedIntermediate,
+            proxy=tuple(MTPROTO)
+    ) as client:
+        client.loop.run_until_complete(run())
